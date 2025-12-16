074fb24fa9d7db5eca593e9e4464165cef0635a0
Rishabh Surana <rishabhjainsurana@gmail.com>
2025-12-16 13:24:20 +0530

    Deployment update

--- Frontend/my-app/src/Home/Home.jsx
+++ Frontend/my-app/src/Home/Home.jsx
@@ -38,7 +38,7 @@ function Home() {
 
     try {
       // BackEnd interaction with FastAPI.
-      const res = await fetch("http://127.0.0.1:8000/ask", {
+      const res = await fetch("https://easylearn-q62w.onrender.com/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
